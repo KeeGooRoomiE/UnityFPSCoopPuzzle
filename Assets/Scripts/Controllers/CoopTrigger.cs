ece93d8c4aebf1f6b9f1d881d951e3ecc62693a1
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2021-02-06 06:42:08 +0700

    Update Coop controller

--- Assets/Scripts/Controllers/CoopTrigger.cs
+++ Assets/Scripts/Controllers/CoopTrigger.cs
@@ -6,12 +6,19 @@ using System.Collections.Generic;
 public class CoopTrigger : MonoBehaviour
 {
     public int ActivePlayer;
+    public int ActiveCamera;
+    public int ActiveMovement;
+
+    public Sprite Player1Cross;
+    public Sprite Player2Cross;
 
     public FirstPersonAIO Player1Controller;
     public FirstPersonAIO Player2Controller;
 
     public Camera Player1Camera;
     public Camera Player2Camera;
+
+    
     
     //public GameObject Player1Head;
     //public GameObject Player2Head;
@@ -19,7 +26,9 @@ public class CoopTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         ActivePlayer = 1;
+        ActivePlayer = 1;
+        ActiveCamera = 1;
+        ActiveMovement = 1;
     }
 
     // Update is called once per frame
@@ -30,11 +39,47 @@ public class CoopTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Tab)) {
             if (ActivePlayer == 1) {
                 ActivePlayer = 2;
+                ActiveMovement = 2;
+                ActiveCamera = 2;
             } 
             else {
                 ActivePlayer = 1;
+                ActiveCamera = 1;
+                ActiveMovement = 1;
             }
         }
+
+        //Pressing Q triggers camera change
+        if (Input.GetKeyUp(KeyCode.Q)) {
+            if (ActiveCamera == 1) {
+                ActiveCamera = 2;
+            } 
+            else {
+                ActiveCamera = 1;
+            }
+        }
+
+        //Pressing E triggers movement change
+        if (Input.GetKeyUp(KeyCode.E)) {
+            if (ActiveMovement == 1) {
+                ActiveMovement = 2;
+            } 
+            else {
+                ActiveMovement = 1;
+            }
+        }
+
+        /*
+        //If Camera and Movement belongs to one setup
+        if (ActiveMovement == 1)  (ActiveCamera == 1) {
+            ActivePlayer = 1;
+        }
+
+        //If Camera and Movement belongs to one setup
+        if (ActiveMovement == 2) and (ActiveCamera == 2) {
+            ActivePlayer = 2;
+        }
+        */
 
         //Changes values between player units
         if (ActivePlayer == 1) {
@@ -43,14 +88,16 @@ public class CoopTrigger : MonoBehaviour
             Player1Camera.enabled = false;
             Player2Camera.enabled = true;
 
-            //Player1Controller.playerCamera = Player2Camera;
-            //Player1Camera.gameObject.SetActive(false);
-
             Player1Controller.useHeadbob = true;
-            Player2Controller.useHeadbob = false;
+            Player2Controller.useHeadbob = true;
 
             Player1Controller.enableCameraMovement = false;
             Player2Controller.enableCameraMovement = true;
+
+            Player1Controller.autoCrosshair = false;
+            Player2Controller.autoCrosshair = true;
+            Player1Controller.Crosshair = Player1Cross;
+            Player2Controller.Crosshair = null;//Player1Cross;
             
             Player1Controller.playerCanMove = true;
             Player2Controller.playerCanMove = false;
@@ -63,21 +110,43 @@ public class CoopTrigger : MonoBehaviour
             //Player2Controller.playerCamera = Player1Camera;
             //Player2Camera.gameObject.SetActive(false);
 
-            Player2Controller.useHeadbob = false;
+            Player2Controller.useHeadbob = true;
             Player1Controller.useHeadbob = true;
 
             Player1Controller.enableCameraMovement = true;
             Player2Controller.enableCameraMovement = false;
 
+            Player1Controller.autoCrosshair = true;
+            Player2Controller.autoCrosshair = false;
+            Player1Controller.Crosshair = null;//Player2Cross;
+            Player2Controller.Crosshair = Player2Cross;
+
             Player1Controller.playerCanMove = false;
             Player2Controller.playerCanMove = true;
         }
-    }
 
-    void FixedUpdate()
-    {
-        
+        if (ActiveCamera == 1) {
 
+            Player1Camera.enabled = false;
+            Player2Camera.enabled = true;
 
+            Player1Controller.enableCameraMovement = false;
+            Player2Controller.enableCameraMovement = true;
+        } else {
+            Player1Camera.enabled = true;
+            Player2Camera.enabled = false;
+
+            Player1Controller.enableCameraMovement = true;
+            Player2Controller.enableCameraMovement = false;
+        }
+
+        if (ActiveMovement == 1) {
+
+            Player1Controller.playerCanMove = true;
+            Player2Controller.playerCanMove = false;
+        } else {
+            Player1Controller.playerCanMove = false;
+            Player2Controller.playerCanMove = true;
+        }
     }
 }
